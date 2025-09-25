62b1a23b28330a222083dcd359d29a021ac4b548
Daniela Cisneros Rosero <ci0816vj@go.minneapolis.edu>
2025-09-25 00:34:21 +0000

    Created Player Class, SuperPlayer Class, Code to test classes

---
+++ Mod5Lab/SuperPlayer.cs
@@ -0,0 +1,11 @@
+namespace Module5Lab
+{
+    internal class SuperPlayer : Player
+    {
+        // Aditional methods specific to SuperPlayer
+        public void Fly()
+        {
+            Console.WriteLine("SuperPlayer is flying!");
+        }// end method
+    } // end class
+}// end namespace
